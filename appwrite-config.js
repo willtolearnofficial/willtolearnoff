8269cf3a89f301b7069f264d5699ab36110fe011
willtolearnofficial <willtolearnofficial@gmail.com>
2026-01-14 14:55:16 +0530

    Delete appwrite-config.js

--- appwrite-config.js
+++
@@ -1,8 +0,0 @@
-const { Client, Account, Functions, ID } = Appwrite;
-
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('696750780007c6fc4cba'); // Corrected Project ID from your dashboard
-
-const account = new Account(client);
-const functions = new Functions(client); // Added Functions to talk to MongoDB
