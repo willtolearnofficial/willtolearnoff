989eb991c044b6f92933434be0132114b11d0cd1
willtolearnofficial <willtolearnofficial@gmail.com>
2026-01-14 14:40:59 +0530

    Update appwrite-config.js

--- appwrite-config.js
+++ appwrite-config.js
@@ -1,12 +1,8 @@
-// Initialize the Appwrite client
-const { Client, Account, Databases, ID } = Appwrite;
+const { Client, Account, Functions, ID } = Appwrite;
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Standard Appwrite Cloud endpoint
-    .setProject('69673baf001051163ba7');         // Your specific Project ID
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('696750780007c6fc4cba'); // Corrected Project ID from your dashboard
 
 const account = new Account(client);
-const databases = new Databases(client);
-
-// Exporting these so you can use them in other files if needed
-// If you're not using modules, you can just keep them as global variables
+const functions = new Functions(client); // Added Functions to talk to MongoDB
